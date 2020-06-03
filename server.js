66a6dbc5a92ab948f62f81d7e4ee7a37a967cafc
Glitch (hello-express) <none>
2020-06-03 03:54:09 +0000

    ⛄️🚧 Checkpoint ./server.js:17077948/2702

--- server.js
+++ server.js
@@ -3,14 +3,18 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
 });
-
+app.get("/todos", (request, response) => {
+  response.send(
+    "<h2> todos list</h2> <br> <ul><li>Di cho</li><li>Nau com</li><li>Rua bat</li><li>Hoc CodersX</li></ul> "
+  );
+});
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);

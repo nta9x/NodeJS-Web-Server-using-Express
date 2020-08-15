c21c5dc8a5292e34dbdfc341ce3f510ca665c06f
nta9x <nguyentuananhk143@gmail.com>
2020-08-15 07:40:18 +0700

    using password mp5

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,15 +1,14 @@
 const db = require('../db');
 const { signedCookie } = require('cookie-parser');
+const md5 = require('md5');
 
 module.exports.login = function(req, res) {
     res.render('auth/login');
 };
 module.exports.postLogin = function(req, res) {
     var email = req.body.email;
-    var password = req.body.password;
-    console.log(email, password)
+    var password = md5(req.body.password);
     var user = db.get('users').find({email: email}).value();
-
     if(!user){
         res.render('auth/login', {
             err:['user does not exist.'],
